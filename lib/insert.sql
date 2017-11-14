780e2f2653e38e7d1a87209c6311467068c60f3b
Kristen Peck <kpeck71@gmail.com>
2017-11-14 02:32:11 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO (name,age,gender,color,temperament,alive) VALUES ('Mr. Chocolate'),
+INSERT INTO (name,age,gender,color,temperament,alive) VALUES ('Rowdy'),
+INSERT INTO (name,age,gender,color,temperament,alive) VALUES ('Tabitha'),
+INSERT INTO (name,age,gender,color,temperament,alive) VALUES ('Sergeant Brown'),
+INSERT INTO (name,age,gender,color,temperament,alive) VALUES ('Melissa'),
+INSERT INTO (name,age,gender,color,temperament,alive) VALUES ('Grinch'),
+INSERT INTO (name,age,gender,color,temperament,alive) VALUES ('Wendy')
