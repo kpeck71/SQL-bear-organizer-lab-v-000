086036d95ddaf9e27206ba12b87e9d97d6a7de54
Kristen Peck <kpeck71@gmail.com>
2017-11-14 02:36:44 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Sergeant Br
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Melissa', 1, 'F', 'brown', 'calm', 0),
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Grinch', 4, 'M', 'green','grumpy',1),
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Wendy', 5, 'F', 'white','sassy',1),
-INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (NULL, 3, 'M', 'brown','murderous',0)
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (NULL, 3, 'M', 'brown','murderous',0);
